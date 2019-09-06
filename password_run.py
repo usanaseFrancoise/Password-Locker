c64e05360ce1b4cfb6df7a5e2620a3dc38b6d427
usanase Francoise <fusanasencoise@gmail.com>
2019-09-06 15:22:47 +0200

    message when you want to exit

--- password_run.py
+++ password_run.py
@@ -148,7 +148,11 @@ def main():
 
                  
          
-            
+            else:
+                print("_"*80)
+                print(' ')
+                print("try Again")
+
 
 
 if __name__ == '__main__':
